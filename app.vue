897c0c3f94acb57084d2dd82ca75dbe7fdf54da8
zhang771 <19939926438@163.com>
2023-09-02 20:22:54 +0800

    chore: init

--- app.vue
+++ app.vue
@@ -1,9 +1,4 @@
 <script setup lang="ts">
-import { appName } from '~/constants'
-
-useHead({
-  title: appName,
-})
 </script>
 
 <template>
@@ -14,14 +9,4 @@ useHead({
 </template>
 
 <style>
-html, body , #__nuxt{
-  height: 100vh;
-  margin: 0;
-  padding: 0;
-}
-
-html.dark {
-  background: #222;
-  color: white;
-}
 </style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,4 @@
 import { pwa } from './config/pwa'
-import { appDescription } from './constants/index'
 
 export default defineNuxtConfig({
   modules: [
@@ -11,8 +10,6 @@ export default defineNuxtConfig({
   ],
 
   experimental: {
-    // when using generate, payload js assets included in sw precache manifest
-    // but missing on offline, disabling extraction it until fixed
     payloadExtraction: false,
     inlineSSRStyles: false,
     renderJsonPayloads: true,
@@ -37,22 +34,6 @@ export default defineNuxtConfig({
       crawlLinks: false,
       routes: ['/'],
       ignore: ['/hi'],
-    },
-  },
-
-  app: {
-    head: {
-      viewport: 'width=device-width,initial-scale=1',
-      link: [
-        { rel: 'icon', href: '/favicon.ico', sizes: 'any' },
-        { rel: 'icon', type: 'image/svg+xml', href: '/nuxt.svg' },
-        { rel: 'apple-touch-icon', href: '/apple-touch-icon.png' },
-      ],
-      meta: [
-        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-        { name: 'description', content: appDescription },
-        { name: 'apple-mobile-web-app-status-bar-style', content: 'black-translucent' },
-      ],
     },
   },
 
